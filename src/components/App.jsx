8eeb7c8f99a46a020aa9edbb3a7405e92351c7d8
YaroslavPlaksii91 <yarosyk@ukr.net>
2022-06-26 15:20:50 +0300

    add lazy download

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useEffect, Suspense, lazy } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { Skeleton } from '@mui/material';
 import AppBar from './AppBar';
-import SignUp from './views/SignUpView';
-import SignIn from './views/SignInView';
-import ContactsView from './views/ContactsView';
-import { authOperations } from 'redux/auth';
+import { authOperations, authSelectors } from 'redux/auth';
 import { ProtectedRoute } from './ProtectedRoute';
 import { PublicRoute } from './PublicRoute';
 
+const SignUp = lazy(() =>
+  import('./views/SignUpView' /* webpackChunkName: "sign-up" */),
+);
+const SignIn = lazy(() =>
+  import('./views/SignInView' /* webpackChunkName: "sign-in" */),
+);
+const ContactsView = lazy(() =>
+  import('./views/ContactsView' /* webpackChunkName: "contacts" */),
+);
+
 const App = () => {
   const dispatch = useDispatch();
+  const isFetchingCurrentUser = useSelector(authSelectors.getIsFetchingCurrent);
 
   useEffect(() => {
     dispatch(authOperations.fetchCurrentUser());
@@ -18,34 +27,47 @@ const App = () => {
 
   return (
     <div>
-      <AppBar />
-      <Routes>
-        <Route
-          path="/register"
-          element={
-            <PublicRoute redirectPath={'/contacts'}>
-              <SignUp />
-            </PublicRoute>
-          }
+      {isFetchingCurrentUser ? (
+        <Skeleton
+          animation={'wave'}
+          variant="rectangular"
+          width={300}
+          height={200}
         />
-        <Route
-          path="/login"
-          element={
-            <PublicRoute redirectPath={'/contacts'}>
-              <SignIn />
-            </PublicRoute>
-          }
-        />
-        <Route
-          path="/contacts"
-          element={
-            <ProtectedRoute redirectPath={'/login'}>
-              <ContactsView />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="*" element={<Navigate to="/register" />} />
-      </Routes>
+      ) : (
+        <>
+          <AppBar />
+          <Suspense fallback={<p>Download...</p>}>
+            <Routes>
+              <Route
+                path="/register"
+                element={
+                  <PublicRoute redirectPath={'/contacts'}>
+                    <SignUp />
+                  </PublicRoute>
+                }
+              />
+              <Route
+                path="/login"
+                element={
+                  <PublicRoute redirectPath={'/contacts'}>
+                    <SignIn />
+                  </PublicRoute>
+                }
+              />
+              <Route
+                path="/contacts"
+                element={
+                  <ProtectedRoute redirectPath={'/login'}>
+                    <ContactsView />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="*" element={<Navigate to="/register" />} />
+            </Routes>
+          </Suspense>
+        </>
+      )}
     </div>
   );
 };
